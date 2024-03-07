8cc8fcff9dc478d06c8bd3f61955432eb4614dec
Nadya163 <132590146+Nadya163@users.noreply.github.com>
2024-03-07 23:19:01 +0600

    Доработала загрузку пользователей

--- src/page/Main/mainPage.jsx
+++ src/page/Main/mainPage.jsx
@@ -5,21 +5,32 @@ function MainPage() {
     const [searchUser, setSearchUser] = useState("");
     const [foundUsers, setFoundUsers] = useState([]);
     const [page, setPage] = useState(1); // Загрузка начиная с первой страницы
+    const [isLoading, setIsLoading] = useState(false); // Состояние загрузки данных
+    const [hasMoreUsers, setHasMoreUsers] = useState(false); // Состояние для определения наличия больше пользователей
     const userPerPage = 20; // Константа для вызова только 20 пользователей
 
-    const handleSearch = async (pageNum) => {
+    const handleSearch = async () => {
+        if (!searchUser) {
+            setFoundUsers([]);
+            setHasMoreUsers(false);
+            return;
+        }
+        setPage(1); // Сбросить номер страницы при новом запросе
+        setFoundUsers([]); // Сбросить список найденных пользователей
+        setIsLoading(true); // Установить состояние загрузки в true
         if (searchUser) {
             try {
                 const response = await fetch(
-                    `https://api.github.com/search/users?q=${searchUser}&per_page=${userPerPage}&page=${pageNum}`,
+                    `https://api.github.com/search/users?q=${searchUser}&per_page=${userPerPage}&page=1`,
                 );
                 const data = await response.json();
-                console.log(data);
-                setFoundUsers([...foundUsers, ...data.items]);
+                setFoundUsers(data.items);
+                setHasMoreUsers(data.total_count > userPerPage);
             } catch (error) {
                 console.error("Error fetching data:", error);
             }
         }
+        setIsLoading(false); // Установить состояние загрузки в false после завершения запроса
     };
 
     // Функция для загрузки следующих 20 пользователей
@@ -27,7 +38,17 @@ function MainPage() {
         const step = 1;
         const nextPage = page + step;
         setPage(nextPage);
-        await handleSearch(nextPage);
+        setIsLoading(true);
+        try {
+            const response = await fetch(
+                `https://api.github.com/search/users?q=${searchUser}&per_page=${userPerPage}&page=${nextPage}`,
+            );
+            const data = await response.json();
+            setFoundUsers([...foundUsers, ...data.items]);
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
+        setIsLoading(false);
     };
 
     return (
@@ -41,8 +62,12 @@ function MainPage() {
                     onChange={(e) => setSearchUser(e.target.value)}
                 />
                 <span />
-                <S.SearchButton type="button" onClick={handleSearch}>
-                    Поиск
+                <S.SearchButton
+                    type="button"
+                    onClick={handleSearch}
+                    disabled={isLoading}
+                >
+                    {isLoading ? "Загрузка..." : "Поиск"}
                 </S.SearchButton>
             </S.SearchItem>
             <S.UserItem>
@@ -58,9 +83,15 @@ function MainPage() {
                     ))}
                 </S.ListOfUsers>
             </S.UserItem>
-            <S.SearchButton type="button" onClick={handleLoadMore}>
-                Загрузить еще
-            </S.SearchButton>
+            {hasMoreUsers && (
+                <S.SearchButton
+                    type="button"
+                    onClick={handleLoadMore}
+                    disabled={isLoading}
+                >
+                    {isLoading ? "Загрузка..." : "Загрузить еще"}
+                </S.SearchButton>
+            )}
         </S.MainContainer>
     );
 }
